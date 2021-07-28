0032391372e3aa175d16746e533f83accc4d7607
Ryan Earles <ryanjayearles@gmail.com>
2021-07-28 12:08:06 -0600

    here's my changes

---
+++ commandsUsed.md
@@ -0,0 +1,2 @@
+#  git clone https://github.com/210726-wvu-net-ext/PO-Ryan-Earles.git
+# 
---
+++ hello.sh
@@ -0,0 +1,4 @@
+#! /usr/bin/bash
+echo "Enter your top three movies "
+read movie1 movie2 movie3
+echo "the three movies are $movie1 $movie2 $movie3"
